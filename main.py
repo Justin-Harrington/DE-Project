e61c734489a1760c1a995683bc0f6e299b06645d
Justin Harrington <justinharrington52@gmail.com>
2025-12-10 18:16:36 -0800

    set up uv and added requests + bs4

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    url = "http://ufcstats.com/statistics/events/completed"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    print(response.headers["Content-Type"])
+
+
+if __name__ == "__main__":
+    main()
